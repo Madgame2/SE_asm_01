f68ecf34eb4cf933ea630e46f016b847bfd87e4c
Anton <astakheyko@bk.ru>
2024-11-05 22:02:23 +0300

    first asm program

---
+++ SE_asm_01/LP_asm.asm
@@ -0,0 +1,39 @@
+.686P													; Для Pinthium Pro и более новых
+.MODEL FLAT, STDCALL									; Модель памяти и соглашение о вызовах
+includelib kernel32.lib				
+
+
+ExitProcess PROTO : DWORD								;Прототип функции из библиотеки Windows c аргументом целое число
+MessageBoxA PROTO : DWORD, : DWORD, : DWORD, :DWORD
+
+
+
+.STACK 4096
+
+
+.CONST
+
+
+.DATA
+MB_OK EQU 0												;MB_OK  = 0
+STR1 DB "Моя первая программа", 0                       ;STR1 =  "Моя первая программа" + \0
+STR2 DB "Привет всем!", 0
+HW DD ?													;Выделяем 4 байта памяти но не инециализируем
+
+.CODE
+
+main PROC
+START :
+		PUSH	MB_OK
+		PUSH	OFFSET STR1
+		PUSH	OFFSET STR2
+		PUSH	HW
+		CALL	MessageBoxA
+
+	push -1
+	call ExitProcess
+
+main ENDP
+
+
+end main
